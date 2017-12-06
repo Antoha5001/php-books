5ad36a9e082524e8014ab80035fb08f4a705af93
Antoha5001 <plantsvzzombi@mail.ru>
2017-12-07 00:16:04 +0700

    123

--- _koterov/gl8/hello.php
+++ _koterov/gl8/hello.php
@@ -1,3 +1,5 @@
 <?php
-
+if($_GET["login"] == 'Anton' && $_GET["password"] == 123){
+	echo "Вам открыт доступ";
+}
 ?>
--- _koterov/gl8/index.php
+++ _koterov/gl8/index.php
@@ -8,13 +8,21 @@
 <h1>First programm</h1>
 <?php
 
-echo $_SERVER['QUERY_STRING'] ?? "";
+echo $_SERVER['SCRIPT_NAME'] ?? "";
 
 ?>
-<form action="hello.php">
+<?php if (!isset($_GET['submit'])){?>
+<form action="<?= $_SERVER['SCRIPT_NAME']?>">
 	Логин: <input type="text" name="login" value=""><br/>
 	Пароль: <input type="text" name="password" value=""><br/>
-	<input type="submit" value="Нажмите на кнопку, чтобы запустить сценарий!">
+	<input type="submit" value="Нажмите на кнопку, чтобы запустить сценарий!" name="submit">
 </form>
+<?php } else{
+	if($_GET['login'] == "Anton" && $_GET['password'] === "123"){
+		echo "Вам открыт доступ";
+	} else {
+		echo "Доступа нет!";
+	}
+} ?>
 </body>
 </html>
