852f8f2ab1405b2debd5261a962168a93cf91eac
Antoha5001 <plantsvzzombi@mail.ru>
2017-12-22 00:08:05 +0700

    123

---
+++ _koterov/gl_18/index.php
@@ -0,0 +1,15 @@
+<?php include_once "libs.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+</head>
+<body>
+<?php
+
+$str = `dir`;
+echo $str;
+?>
+</body>
+</html>
