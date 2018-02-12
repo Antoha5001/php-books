cebc207d21275be0a1c31945778a80ce1e179b6a
Antoha5001 <plantsvzzombi@mail.ru>
2018-02-13 00:34:59 +0700

    123

--- _koterov/gl-44/src/ISPager/FilePager.php
+++ _koterov/gl-44/src/ISPager/FilePager.php
@@ -26,9 +26,45 @@
 		}
 
 		public function getItems(){
+			$current_page = $this->getCurentPage();
+			$total_line_on_page = $this->getItemsCount();
+			$total_page = $this->getPagesCount();
+
+			if($current_page <= 0 || $current_page > $total_line_on_page){
+				return 0;
+			}
+			$arr = [];
+			$file = fopen($this->filename, 'r');
+
+			if(!$file) return 0;
+
+			$first = ($current_page - 1) * $this->getItemsPerPage();
+
+			for ($i=0; $i<$total_line_on_page; $i++){
+				$str = fgetc($file,10000);
+
+				if($i < $first) continue;
+
+				if($i > $first + $this->getItemsPerPage()-1) break;
+
+				$arr[]= $str;
+
+			}
 
 		}
-		public function getItemsCount(){
 
+		public function getItemsCount(){
+			$countline = 0;
+
+			$file = fopen($this->filename, 'r');
+
+			if($file){
+				while (!feof($file)){
+					fgetc($file, 10000);
+					$countline++;
+				}
+				fclose($file);
+			}
+			return $countline;
 		}
 	}
