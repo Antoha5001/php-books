65eb647b717e3196cebe639ccee8fb4519099680
Antoha5001 <plantsvzzombi@mail.ru>
2018-03-01 16:18:34 +0700

    48

--- _koterov/gl-47/album.php
+++ _koterov/gl-47/album.php
@@ -1,19 +1,47 @@
 <?php
 	$img_dir = 'img';
-	mkdir($img_dir,0777);
+	if (!is_dir($img_dir)) {
 
-	if(isset($_REQUEST['doUpload'])){
+		mkdir($img_dir, 0777);
+	}
+
+	if ($_REQUEST['doUpload']) {
 		$data = $_FILES['file'];
-		$tmp = $data['tmp-name'];
+		$tmp = $data['tmp_name'];
 
-		if(is_uploaded_file($tmp)){
+
+		if (is_uploaded_file($tmp)) {
 			$info = getimagesize($tmp);
 
-			if (preg_match('{image/(.*)}is', $info['mime'], $p)){
+			if (preg_match('{image/(.*)}is', $info['mime'], $p)) {
+				$name = "$img_dir/" . time() . "." . $p[1];
 
+				move_uploaded_file($tmp, $name);
+			} else {
+				echo "<h2>Попытка добавить файл недопустимого формата!</h2>";
 			}
+		} else {
+			echo "<h2>Ошибка закачки # {$data['error']}</h2>";
 		}
 	}
+
+	$photos = array();
+
+	foreach (glob("$img_dir/*") as $path) {
+		$size = getimagesize($path);
+		$tm = filemtime($path);
+
+		$photos[$tm] = [
+			'time' => $tm,
+			'name' => basename($path),
+			'url' => $path,
+			'width' => $size[0],
+			'height' => $size[1],
+			'wh' => $size[3]
+		];
+	}
+
+	krsort($photos);
 
 ?>
 
@@ -26,5 +54,16 @@
 </head>
 <body>
 
+<form action="<?= $_SERVER['SCRIPT_NAME']?>" method="post" enctype="multipart/form-data">
+	<input type="file" name="file"><br>
+	<input type="submit" name="doUpload" value="Закачать фото">
+	<hr>
+</form>
+
+<?php foreach ($photos as $n=>$img) {?>
+
+	<p><img src="<?=$img['url']?>" <?=$img['wh']?> alt="Добавлена <?=date('d.m.Y H:i:s',$img['time'])?>"></p>
+	
+<?php } ?>
 </body>
 </html>
